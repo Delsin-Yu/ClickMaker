9027449f832dfa4ee4f8b1d1255bfeccc1a04dc3
DE YU <71481700+Delsin-Yu@users.noreply.github.com>
2025-09-16 22:04:22 +0800

    Implment bar number reporting

--- ClickMaker/Program.cs
+++ ClickMaker/Program.cs
@@ -4,13 +4,12 @@ using System.Runtime.InteropServices;
 using System.Text.Json;
 using System.Text.Json.Schema;
 using System.Text.Json.Serialization;
+using System.Transactions;
 using Melanchall.DryWetMidi.Core;
 using Melanchall.DryWetMidi.Interaction;
 using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
 
-const string primaryClickPath = "ClickPrimary.wav";
-const string secondaryClickPath = "ClickSecondary.wav";
 const string configFilePath = "Config.json";
 
 if (!File.Exists(configFilePath))
@@ -33,6 +32,8 @@ var config = JsonSerializer.Deserialize(configJson, SerializerContext.Default.So
 if (config.Configs.Length == 0)
     return;
 
+var cache = new Dictionary<int, AudioFileReaderAllocation>();
+
 foreach (var (file, importantBars) in config.Configs)
 {
     var clicks = CreateClickInfo(file);
@@ -40,12 +41,16 @@ foreach (var (file, importantBars) in config.Configs)
         CollectionsMarshal.AsSpan(clicks),
         importantBars.AsSpan(),
         Path.ChangeExtension(file, "click.wav"),
-        primaryClickPath,
-        secondaryClickPath
+        cache
     );
 }
 
+foreach (var allocation in cache.Values) allocation.Dispose();
+cache.Clear();
+
 return;
+
+const int PrepareTimes = 2;
 
 static List<ClickInfo> CreateClickInfo(string midiFilePath)
 {
@@ -71,20 +76,19 @@ static List<ClickInfo> CreateClickInfo(string midiFilePath)
     var oneBeatMusicalLength = GetOneBeatMusicalLength(currentTimeSignature);
 
     var oneBeatMetricLength = LengthConverter.ConvertTo<MetricTimeSpan>(oneBeatMusicalLength, 0, sourceTempoMap);
-
-    const int prewarmTimes = 2;
     
-    for (var i = 0; i < currentTimeSignature.Numerator * prewarmTimes; i++)
+    for (var i = 0; i < currentTimeSignature.Numerator * PrepareTimes; i++)
     {
-        clickInfos.Add(new(i * oneBeatMetricLength.TotalMicroseconds, i % currentTimeSignature.Numerator == 0 ? ClickType.PreparePrimary : ClickType.PrepareSecondary));
+        clickInfos.Add(new((ulong)(i * oneBeatMetricLength.TotalMicroseconds),
+            i % currentTimeSignature.Numerator == 0 ? ClickType.PreparePrimary : ClickType.PrepareSecondary));
     }
 
-    var offsetMicroseconds = oneBeatMetricLength.TotalMicroseconds * currentTimeSignature.Numerator * prewarmTimes;
+    var offsetMicroseconds = oneBeatMetricLength.TotalMicroseconds * currentTimeSignature.Numerator * PrepareTimes;
     long currentMidiTime = 0;
     var remainingBeats = currentTimeSignature.Numerator;
     while (currentMidiTime < maxMidiClicks)
     {
-        clickInfos.Add(new(TimeConverter.ConvertTo<MetricTimeSpan>(currentMidiTime, sourceTempoMap).TotalMicroseconds + offsetMicroseconds,
+        clickInfos.Add(new((ulong)(TimeConverter.ConvertTo<MetricTimeSpan>(currentMidiTime, sourceTempoMap).TotalMicroseconds + offsetMicroseconds),
             remainingBeats == currentTimeSignature.Numerator
                 ? ClickType.Primary
                 : ClickType.Secondary));
@@ -108,7 +112,7 @@ static List<ClickInfo> CreateClickInfo(string midiFilePath)
     }
     
     var lastClickMicrosecond = TimeConverter.ConvertTo<MetricTimeSpan>(currentMidiTime, sourceTempoMap).TotalMicroseconds + offsetMicroseconds;
-    clickInfos.Add(new(lastClickMicrosecond, ClickType.Final));
+    clickInfos.Add(new((ulong)lastClickMicrosecond, ClickType.Final));
 
     return clickInfos;
 }
@@ -118,61 +122,103 @@ static MusicalTimeSpan GetOneBeatMusicalLength(TimeSignature timeSignature)
     return new(1, timeSignature.Denominator);
 }
 
-static void CreateAudioTrack(ReadOnlySpan<ClickInfo> clickInfo, ReadOnlySpan<int> importantBarNumber, string dstPath, string primaryClickPath, string secondaryClickPath)
+static void CreateAudioTrack(ReadOnlySpan<ClickInfo> clickInfo, ReadOnlySpan<int> importantBarNumber, string dstPath,
+    Dictionary<int, AudioFileReaderAllocation> cache)
 {
-    var sampleSequence = new List<ISampleProvider>();
-    var currentMicrosecond = 0L;
-    foreach (var click in clickInfo)
+    var sequencer = new WaveSequencer();
+    const uint VoiceOffset = 120_000;
+    var barNumber = 0;
+    var prepareVoiceNumber = 1;
+    var remainingCountDownBars = PrepareTimes;
+    var importantSet = new HashSet<int>();
+    foreach (var bar in importantBarNumber)
     {
-        AudioFileReader clickSample = click.Type switch
-        {
-            ClickType.Primary => new(primaryClickPath),
-            ClickType.Secondary => new(secondaryClickPath),
-            ClickType.PreparePrimary => new(primaryClickPath),
-            ClickType.PrepareSecondary => new(secondaryClickPath),
-            ClickType.Final => new(primaryClickPath),
-            _ => throw new UnreachableException()
-        };
-
-        if (sampleSequence.Count == 0)
-        {
-            sampleSequence.Add(clickSample);
-        }
-        else
-        {
-            var lastSample = sampleSequence[^1];
-            TimeSpan lastSampleDuration;
-            switch (lastSample)
-            {
-                case AudioFileReader fileSample:
-                    lastSampleDuration = fileSample.TotalTime;
-                    break;
-                case OffsetSampleProvider offsetSample:
-                    lastSampleDuration = ((AudioFileReader)offsetSample.UnsafeAccessInternalSampleProvider()).TotalTime;
-                    break;
-                default:
-                    throw new UnreachableException();
-            }
-            
-            var offsetSampleProvider = new OffsetSampleProvider(clickSample);
-            var delayAmount = TimeSpan.FromMicroseconds(click.Microsecond - currentMicrosecond) - lastSampleDuration;
-            offsetSampleProvider.DelayBy = delayAmount;
-            sampleSequence.Add(offsetSampleProvider);
-        }
-
-        currentMicrosecond = click.Microsecond;
+        importantSet.Add(bar);
+        importantSet.Add(bar - 1);
+        importantSet.Add(bar - 2);
     }
 
-    var concatenatingSampleProvider = new ConcatenatingSampleProvider(sampleSequence);
-    // var output = new DirectSoundOut(1024);
-    // output.Init(concatenatingSampleProvider);
-    // output.Play();
-    // while (output.PlaybackState == PlaybackState.Playing)
-    // {
-    //     Thread.Sleep(2000);
-    // }
+    foreach (var click in clickInfo)
+    {
+        const string primaryClickPath = "ClickPrimary.wav";
+        const string secondaryClickPath = "ClickSecondary.wav";
+        AudioFileReader clickSample;
+        AudioFileReader? voiceSample = null;
+        switch (click.Type)
+        {
+            case ClickType.Primary:
+                barNumber++;
+                clickSample = new(primaryClickPath);
+                if (importantSet.Contains(barNumber)) voiceSample = GetNumberVoice(barNumber, cache);
+                break;
+            case ClickType.Secondary:
+                clickSample = new(secondaryClickPath);
+                break;
+            case ClickType.PreparePrimary:
+                remainingCountDownBars--;
+                prepareVoiceNumber = 1;
+                clickSample = new(primaryClickPath);
+                if(remainingCountDownBars == 0) voiceSample = GetNumberVoice(1, cache);
+                break;
+            case ClickType.PrepareSecondary:
+                prepareVoiceNumber++;
+                clickSample = new(secondaryClickPath);
+                if(remainingCountDownBars == 0) voiceSample = GetNumberVoice(prepareVoiceNumber, cache);
+                break;
+            case ClickType.Final:
+                clickSample = new(primaryClickPath);
+                break;
+            default:
+                throw new UnreachableException();
+        }
 
-    WaveFileWriter.CreateWaveFile16(dstPath, concatenatingSampleProvider);
+        sequencer.AddSample(clickSample, (long)(click.Microsecond + VoiceOffset));
+        if (voiceSample == null) continue;
+        sequencer.AddSample(voiceSample, (long)click.Microsecond);
+    }
+    WaveFileWriter.CreateWaveFile16(dstPath, sequencer.Bake());
+}
+
+static AudioFileReader GetNumberVoice(int number, Dictionary<int, AudioFileReaderAllocation> cachedAllocations)
+{
+    ArgumentOutOfRangeException.ThrowIfGreaterThan(number, 9999);
+    ArgumentOutOfRangeException.ThrowIfLessThan(number, 1);
+    if (cachedAllocations.TryGetValue(number, out var allocation))
+        return allocation.CreateReader();
+    var path = Path.Combine("VoiceBank", $"{number:D4}.mp3");
+    allocation = new(path);
+    cachedAllocations[number] = allocation;
+    return allocation.CreateReader();
+}
+
+public readonly struct AudioFileReaderAllocation : IDisposable
+{
+    private readonly string _tempFilePath;
+    private readonly Stack<AudioFileReader> _activeReaders = new();
+    
+    public AudioFileReaderAllocation(string path)
+    {
+        var sourceReader = new AudioFileReader(path);
+        var tmpDir = Path.GetTempPath();
+        _tempFilePath = Path.Combine(tmpDir, $"{Guid.NewGuid()}.wav");
+        WaveFileWriter.CreateWaveFile16(_tempFilePath, sourceReader);
+        sourceReader.Dispose();
+    }
+    
+    public AudioFileReader CreateReader()
+    {
+        var reader = new AudioFileReader(_tempFilePath);
+        _activeReaders.Push(reader);
+        return reader;
+    }
+
+    public void Dispose()
+    {
+        while (_activeReaders.TryPop(out var reader)) 
+            reader.Dispose();
+        if(!File.Exists(_tempFilePath)) return;
+        File.Delete(_tempFilePath);
+    }
 }
 
 public enum ClickType
@@ -185,28 +231,11 @@ public enum ClickType
 }
 
 internal readonly record struct ClickInfo(
-    long Microsecond,
+    ulong Microsecond,
     ClickType Type
 );
 
 internal record struct TimeSignatureEvent(long MidiTime, TimeSignature TimeSignature);
-
-internal record struct TempoEvent(long MidiTime, Tempo Tempo);
-
-public static class Accessor
-{
-    public static ISampleProvider UnsafeAccessInternalSampleProvider(this OffsetSampleProvider provider) =>
-        AccessorImpl1(provider);
-    
-    public static void UnsafeChangeTime(this MetricTimeSpan metricTimeSpan, long totalMicroseconds) =>
-        AccessorImpl2(metricTimeSpan) = new(totalMicroseconds * 10);
-    
-    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "sourceProvider")]
-    private static extern ref ISampleProvider AccessorImpl1(OffsetSampleProvider provider);
-    
-    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_timeSpan")]
-    private static extern ref TimeSpan AccessorImpl2(MetricTimeSpan time);
-}
 
 public record SoundConfig(string MidiFilePath, int[] ImportantBars);
 
@@ -215,3 +244,65 @@ public record struct SoundConfigProfile(SoundConfig[] Configs);
 [JsonSerializable(typeof(SoundConfigProfile))]
 [JsonSourceGenerationOptions(WriteIndented = true)]
 public partial class SerializerContext : JsonSerializerContext;
+
+public class WaveSequencer
+{
+    private record struct Sample(AudioFileReader Provider, long MicrosecondPosition);
+    private readonly List<Sample> _samples = [];
+    
+    public void AddSample(AudioFileReader sample, long microsecondPosition)
+    {
+        _samples.Add(new(sample, microsecondPosition));
+    }
+
+    public MixingSampleProvider Bake()
+    {
+        _samples.Sort((a, b) => Comparer<long>.Default.Compare(a.MicrosecondPosition, b.MicrosecondPosition));
+        var tracks = new List<AudioTrack>();
+        
+        foreach (var sample in _samples)
+        {
+            var placed = false;
+            foreach (var track in tracks)
+            {
+                if (!track.TryAppendSample(sample.Provider, sample.MicrosecondPosition)) continue;
+                placed = true;
+                break;
+            }
+
+            if (placed) continue;
+            var newTrack = new AudioTrack();
+            if (!newTrack.TryAppendSample(sample.Provider, sample.MicrosecondPosition))
+            {
+                throw new UnreachableException();
+            }
+            tracks.Add(newTrack);
+        }
+
+        return new(tracks
+            .Select(track => track.CreateSampleProvider())
+            .ToList());
+    }
+
+    private class AudioTrack
+    {
+        private long _headMicrosecondPosition;
+        private readonly List<OffsetSampleProvider> _samples = [];
+
+        public bool TryAppendSample(AudioFileReader provider, long requestedStartPosition)
+        {
+            var offset = requestedStartPosition - _headMicrosecondPosition;
+            if (offset < 0) return false;
+            var offsetProvider = new OffsetSampleProvider(provider)
+            {
+                DelayBy = TimeSpan.FromMicroseconds(offset)
+            };
+            _samples.Add(offsetProvider);
+            _headMicrosecondPosition += offset;
+            _headMicrosecondPosition += (long)provider.TotalTime.TotalMicroseconds;
+            return true;
+        }
+
+        public ConcatenatingSampleProvider CreateSampleProvider() => new(_samples);
+    }
+}
